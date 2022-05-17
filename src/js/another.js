c20e171585b5a36d43dc0d17c95b328089d3e7de
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2022-05-17 21:21:28 +0200

    commit

--- src/js/another.js
+++ src/js/another.js
@@ -1,1 +1,13 @@
-console.log('hello another');
+export function mov(firstRegister, secondRegister) {
+  if (firstRegister.innerHTML != 0) {
+    secondRegister.innerHTML = firstRegister.innerHTML;
+    firstRegister.innerHTML = 0;
+  } else if (firstRegister.innerHTML == 0) {
+    secondRegister.innerHTML = 0;
+  }
+}
+export function xchg(firstRegister, secondRegister) {
+  const temp = firstRegister.innerHTML;
+  firstRegister.innerHTML = secondRegister.innerHTML;
+  secondRegister.innerHTML = temp;
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,155 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
+import { mov, xchg } from "./another";
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+//rejestry
+let AX = document.querySelector(".section__paragraph--registers-AX");
+let BX = document.querySelector(".section__paragraph--registers-BX");
+let CX = document.querySelector(".section__paragraph--registers-CX");
+let DX = document.querySelector(".section__paragraph--registers-DX");
 
-/* place your code below */
+//mov/xchg
+//AX
+const movAXBX = document.querySelector(".section__button--movAXBX");
+const movAXCX = document.querySelector(".section__button--movAXCX");
+const movAXDX = document.querySelector(".section__button--movAXDX");
+const xchgAXBX = document.querySelector(".section__button--xchgAXBX");
+const xchgAXCX = document.querySelector(".section__button--xchgAXCX");
+const xchgAXDX = document.querySelector(".section__button--xchgAXDX");
 
-console.log('HELLO 🚀')
+//BX
+const movBXAX = document.querySelector(".section__button--movBXAX");
+const movBXCX = document.querySelector(".section__button--movBXCX");
+const movBXDX = document.querySelector(".section__button--movBXDX");
+const xchgBXAX = document.querySelector(".section__button--xchgBXAX");
+const xchgBXCX = document.querySelector(".section__button--xchgBXCX");
+const xchgBXDX = document.querySelector(".section__button--xchgBXDX");
+
+//CX
+const movCXAX = document.querySelector(".section__button--movCXAX");
+const movCXBX = document.querySelector(".section__button--movCXBX");
+const movCXDX = document.querySelector(".section__button--movCXDX");
+const xchgCXAX = document.querySelector(".section__button--xchgCXAX");
+const xchgCXBX = document.querySelector(".section__button--xchgCXBX");
+const xchgCXDX = document.querySelector(".section__button--xchgCXDX");
+
+//DX
+const movDXAX = document.querySelector(".section__button--movDXAX");
+const movDXBX = document.querySelector(".section__button--movDXBX");
+const movDXCX = document.querySelector(".section__button--movDXCX");
+const xchgDXAX = document.querySelector(".section__button--xchgDXAX");
+const xchgDXBX = document.querySelector(".section__button--xchgDXBX");
+const xchgDXCX = document.querySelector(".section__button--xchgDXCX");
+
+//rejestry random/reset
+const registersRandom = document.querySelector(
+  ".section__button--random-register"
+);
+const registersReset = document.querySelector(
+  ".section__button--reset-register"
+);
+registersRandom.addEventListener("click", () => {
+  const randomValueAX = Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .substring(0, 4);
+  const randomValueBX = Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .substring(0, 4);
+  const randomValueCX = Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .substring(0, 4);
+  const randomValueDX = Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .substring(0, 4);
+  AX.innerHTML = randomValueAX.toUpperCase();
+  BX.innerHTML = randomValueBX.toUpperCase();
+  CX.innerHTML = randomValueCX.toUpperCase();
+  DX.innerHTML = randomValueDX.toUpperCase();
+});
+registersReset.addEventListener("click", () => {
+  AX.innerHTML = 0;
+  BX.innerHTML = 0;
+  CX.innerHTML = 0;
+  DX.innerHTML = 0;
+});
+
+//mov/xchg buttony
+//AX
+movAXBX.addEventListener("click", () => {
+  mov(AX, BX);
+});
+movAXCX.addEventListener("click", () => {
+  mov(AX, CX);
+});
+movAXDX.addEventListener("click", () => {
+  mov(AX, DX);
+});
+xchgAXBX.addEventListener("click", () => {
+  xchg(AX, BX);
+});
+xchgAXCX.addEventListener("click", () => {
+  xchg(AX, CX);
+});
+xchgAXDX.addEventListener("click", () => {
+  xchg(AX, DX);
+});
+//BX
+movBXAX.addEventListener("click", () => {
+  mov(BX, AX);
+});
+movBXCX.addEventListener("click", () => {
+  mov(BX, CX);
+});
+movBXDX.addEventListener("click", () => {
+  mov(BX, DX);
+});
+xchgBXAX.addEventListener("click", () => {
+  xchg(BX, AX);
+});
+xchgBXCX.addEventListener("click", () => {
+  xchg(BX, CX);
+});
+xchgBXDX.addEventListener("click", () => {
+  xchg(BX, DX);
+});
+//CX
+movCXAX.addEventListener("click", () => {
+  mov(CX, AX);
+});
+movCXBX.addEventListener("click", () => {
+  mov(CX, BX);
+});
+movCXDX.addEventListener("click", () => {
+  mov(CX, DX);
+});
+xchgCXAX.addEventListener("click", () => {
+  xchg(CX, AX);
+});
+xchgCXBX.addEventListener("click", () => {
+  xchg(CX, BX);
+});
+xchgCXDX.addEventListener("click", () => {
+  xchg(CX, DX);
+});
+//DX
+movDXAX.addEventListener("click", () => {
+  mov(DX, AX);
+});
+movDXBX.addEventListener("click", () => {
+  mov(DX, BX);
+});
+movDXCX.addEventListener("click", () => {
+  mov(DX, CX);
+});
+xchgDXAX.addEventListener("click", () => {
+  xchg(DX, AX);
+});
+xchgDXBX.addEventListener("click", () => {
+  xchg(DX, BX);
+});
+xchgDXCX.addEventListener("click", () => {
+  xchg(DX, CX);
+});
+
+//push pop
+// const pushAX = document.querySelector(".section__button--pushAX");
+// pushAX.addEventListener();
